d233403e9ceaa73d787bd103f7b97a645179cbb9
Michael Cavaliere <michaelc@microsoft.com>
2018-07-03 16:21:23 -0700

    added some logic

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 
 let productname = "Hammer:";
 console.log(productname);
+
+
+class simpleCounter{
+     count(){
+        let count = 5;
+
+    for (let i = 1; i<= 7; i++){
+        document.write(i.toString() + '<br>')
+    }
+        console.log('All done!');
+    }
+
+}
+
+let count = new simpleCounter
+count.count();
